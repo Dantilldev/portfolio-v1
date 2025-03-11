1be394475c3117cbcbb850eda189e6e16f3a8640
Daniel Tilleria <daniel.tilleria@hotmail.com>
2025-03-11 10:06:23 +0100

    first commit

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="flex items-center w-full min-h-16">
+      <div>
+        <a className="text-lg hover:animate-jello hover:text-secondary min-w-[9rem] inline-block ">
+          Daniel Tilleria
+        </a>
+      </div>
+    </div>
+  );
+}
