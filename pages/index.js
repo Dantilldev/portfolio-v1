a36eae671b93cab90653051c5fbf405a119e6518
Daniel Tilleria <daniel.tilleria@hotmail.com>
2025-03-11 10:10:49 +0100

    Testing updating

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="w-screen min-h-screen">
-      <h1 className="font-bold">Välkommen</h1>
+      <h1 className="font-bold">Välkommen Uppdatering</h1>
     </div>
   );
 }
